d0f773ce6c55e0abb1a4380e74c3e26907aa97a1
“antoanparashkevov” <tonkata1505@gmail.com>
2022-10-30 20:33:18 +0200

    User nav condition statements for guest and logged in user

--- middlewares/session.js
+++ middlewares/session.js
@@ -6,6 +6,7 @@ module.exports = () => (req,res,next)=> {
         try {
             const userData = verifySession(token)
             req.user = userData;
+            res.locals.username = userData.username;
             console.log('Verified User Data >>> ', req.user)
         } catch (err) {
             //in case the cookie is invalid
